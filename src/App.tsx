3b189759f4da65c8e7152c733b8d482bb8e101a6
Ahmer Malik <ahmermalik@Ahmers-Mac-mini.local>
2023-01-25 12:21:29 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import logo from './bcblogo.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+
+<h2> Uh oh! You've reached here by mistake trying to reach <a href="https://blockchainbets.club/">Blockchain Bets!</a>  </h2>
+
+<p> 🔗 Important links to $BCB platform 🔗 </p>
+
+<ul> 
+  <ol><a href="https://blockchainbets.app/">$BCB Main </a></ol>
+  <ol><a href="https://blockchainbets.club/">$BCB Betting </a></ol>
+  <ol><a href="https://staking.blockchainbets.app/">$BCB Staking </a></ol>
+
+</ul>
+<p> 🐣 Important links to $BCB socials 🐣 </p>
+<ul>
+<dd><a href="https://t.me/BCBerc20"> $BCB Telegram </a></dd>
+  <dd><a href="https://twitter.com/BCBerc20"> $BCB Twitter </a></dd>
+</ul>
+
+
+
+
+
+
+
+
+
+  
+  <h2> 👇 Follow the steps below to ensure your safety in crypto👇 </h2>
+<ul> 
+  <dd>- Always bookmark the correct URL and only visit the bookmarked URL. <br/> Spelling mistakes happen.</dd>
+  <br/>
+  <dd>- Never give access to a web-app that automaticaly tries to connect to your wallet. <br/> i.e. "give xyz app access to xyz coin"</dd>
+
+</ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
